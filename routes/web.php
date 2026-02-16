b71328aa6c23d3d4e856671446391b5fcbb6bb5d
mariomo16 <mariomo16.cieza@gmail.com>
2026-02-16 18:37:25 +0100

    install:api

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'name' => config('app.name'),
+        'description' => 'LAN Legacy REST API',
+        'version' => '0.1.0',
+        'status' => 'Active',
+        'environment' => app()->environment(),
+    ]);
 });
